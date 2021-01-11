d04671b20f1961032b3ee7432df0f463e60adebc
ReTr0w0rLd <72004747+ReTr0w0rLd@users.noreply.github.com>
2021-01-11 09:56:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace focivbk2021
+{
+    class Program
+    {
+        struct orszagok
+        {
+            public string ország;
+            public string részvétel;
+            public string legelső;
+            public string legutóbbi;
+            public string legjobb;
+        }
+        static orszagok[] orszag = new orszagok[80];
+        static void Main(string[] args)
+        {
+            string[] fajlbol = File.ReadAllLines("focivbk2021.txt");
+            int sorokszama = 0;
+            for (int k = 1; k < fajlbol.Count(); k++)
+            {
+                string[] egysordarabolva = fajlbol[k].Split(';');
+                orszag[sorokszama].ország = egysordarabolva[0];
+                orszag[sorokszama].részvétel = egysordarabolva[1];
+                orszag[sorokszama].legelső = egysordarabolva[2];
+                orszag[sorokszama].legutóbbi = egysordarabolva[3];
+                orszag[sorokszama].legjobb = egysordarabolva[4];
+
+                sorokszama++;
+            }
+
+            Console.WriteLine("Versenysorozat a fájlból");
+            int orszagokszama = sorokszama;
+            Console.WriteLine("Ország                   Részvételek száma                   Legelső részvétel                   Legutóbbi részvétel                 Legjobb részvétel");
+            for (int i = 0; i < orszagokszama; i++);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("1. feladat");
+            Console.WriteLine(" ");
+            int[] tomb = { };
+            
+        }
+    }
+}
+
